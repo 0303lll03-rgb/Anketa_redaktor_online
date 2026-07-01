5213d42b9dcdf79063b49face7b4645ab23bbf7c
0303lll03-rgb <0303lll03@gmail.com>
2026-07-01 15:09:20 +0300

    Delete open-editor.cmd

--- open-editor.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "CRM editor server" "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "%~dp0crm-proxy.ps1"
-timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:8787/index.html"
